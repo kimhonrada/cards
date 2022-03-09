356b0e256be52ed68f5c399ddd72dd5ac4855131
Kim Honrada <kimhonrada21@gmail.com>
2022-03-09 15:58:34 -0500

    Add initial card component with tests

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Card from "./Components/Card";
+
+const App = () => {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>
+        Card Component made with React
+      </h1>
+      <Card />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div id='card'>
+      <header>Word of the day</header>
+      <h4 id='word'>Be-ne-vo-lent</h4>
+      <h6 id='word-type'>adjective</h6>
+      <p id='meaning'>well meaning and kindly</p>
+      <button>Learn More</button>
+    </div>
+  )
+}
+
+export default Card;
